c68d9de1266eeb1492f2911190ed563158230bec
Phongdnh-uit <dangnguyenhuyphong@gmail.com>
2025-07-23 12:47:38 +0700

    🚧 build(app): axios config

---
+++ src/constants/appConstant.ts
@@ -0,0 +1,2 @@
+export const ACCESS_TOKEN_STORAGE_KEY = "token";
+export const REFRESH_TOKEN_STORAGE_KEY = "refreshToken";
--- src/services/authService.ts
+++ src/services/authService.ts
@@ -3,19 +3,35 @@ import type { LoginRequestType } from "@/types/auth";
 import { LoginResponseSchema } from "@/types/auth";
 import axios from "axios";
 
+const BACKEND_URL =
+  import.meta.env.VITE_BACKEND_URL || "http://localhost:/api/v1";
+
+const AUTH_URL = BACKEND_URL + "/auth";
+
 export const login = async (
-    request: LoginRequestType,
+  request: LoginRequestType,
 ): Promise<ApiResponseType<typeof LoginResponseSchema>> => {
-    const response = await axios.post(
-        "http://localhost:8080/api/v1/auth/login",
-        request,
-    );
+  const response = await axios.post(AUTH_URL + `/login`, request);
 
-    const LoginApiResponseSchema = ApiResponseSchema(LoginResponseSchema);
+  const LoginApiResponseSchema = ApiResponseSchema(LoginResponseSchema);
 
-    const result = LoginApiResponseSchema.safeParse(response.data);
-    if (!result.success) {
-        throw new Error("Invalid response from server");
-    }
-    return result.data;
+  const result = LoginApiResponseSchema.safeParse(response.data);
+  if (!result.success) {
+    throw new Error("Invalid response from server");
+  }
+  return result.data;
+};
+
+export const refreshAccessToken = async (request: {
+  refreshToken: string;
+}): Promise<ApiResponseType<typeof LoginResponseSchema>> => {
+  const response = await axios.post(AUTH_URL + `/refresh-token`, request);
+
+  const LoginApiResponseSchema = ApiResponseSchema(LoginResponseSchema);
+
+  const result = LoginApiResponseSchema.safeParse(response.data);
+  if (!result.success) {
+    throw new Error("Invalid response from server");
+  }
+  return result.data;
 };
